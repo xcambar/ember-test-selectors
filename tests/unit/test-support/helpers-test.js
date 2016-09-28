e3ddd59478f48020372967c23701454e275e66ae
Chris Krycho <chris@chriskrycho.com>
2016-09-28 08:08:44 -0400

    Check `value` argument for absence, not falsiness. (#24)

--- tests/unit/test-support/helpers-test.js
+++ tests/unit/test-support/helpers-test.js
@@ -5,6 +5,7 @@ module('test-support helpers');
 
 test('expands a selector name and attribute value corretly', function(assert) {
   assert.equal(testSelector('selector', 'welcome-text'), '[data-test-selector="welcome-text"]');
+  assert.equal(testSelector('selector', 0), '[data-test-selector="0"]');
 });
 
 test('expands a selector name without attribute value corretly', function(assert) {

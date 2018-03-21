3a43723618b923124ee43ab22aacfb9f1522d629
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-03-21 16:33:03 +0100

    tests/version-checks: Use `hasEmberVersion()` function
    see https://github.com/emberjs/ember-test-helpers/blob/v0.7.20/addon-test-support/%40ember/test-helpers/has-ember-version.js

--- tests/dummy/app/version-checks.js
+++ tests/dummy/app/version-checks.js
@@ -2,22 +2,20 @@ import Ember from 'ember';
 
 const { VERSION } = Ember;
 
-const EMBERS_WITHOUT_POSITIONAL_PARAMS = [
-  '1.12',
-  '1.11',
-];
+export const hasPositionalParams = hasEmberVersion(1, 13);
+export const hasReliablePositionalParams = hasEmberVersion(2, 3);
 
-const EMBERS_WITHOUT_RELIABLE_POSITIONAL_PARAMS = [
-  '2.2',
-  '2.1',
-  '2.0',
-  '1.13',
-  '1.12',
-  '1.11',
-];
-
-export const hasPositionalParams = !EMBERS_WITHOUT_POSITIONAL_PARAMS
-  .some(version => VERSION.indexOf(`${version}.`) === 0);
-
-export const hasReliablePositionalParams = !EMBERS_WITHOUT_RELIABLE_POSITIONAL_PARAMS
-  .some(version => VERSION.indexOf(`${version}.`) === 0);
+/**
+  Checks if the currently running Ember version is greater than or equal to the
+  specified major and minor version numbers.
+  @private
+  @param {number} major the major version number to compare
+  @param {number} minor the minor version number to compare
+  @returns {boolean} true if the Ember version is >= MAJOR.MINOR specified, false otherwise
+*/
+export function hasEmberVersion(major, minor) {
+  let numbers = VERSION.split('-')[0].split('.');
+  let actualMajor = parseInt(numbers[0], 10);
+  let actualMinor = parseInt(numbers[1], 10);
+  return actualMajor > major || (actualMajor === major && actualMinor >= minor);
+}

dd9c2213a45d066fad83c331032d60650af422ee
mathieupoteriepeopledoc <mathieu.poterie@people-doc.com>
2018-08-29 11:09:46 +0200

    test: fixed test by writing two different test according to the stripTestSelectors boolean set in the config

--- tests/integration/bind-data-test-attributes-test.js
+++ tests/integration/bind-data-test-attributes-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import config from 'dummy/config/environment';
 
 moduleForComponent(
   'print-test-attributes',
@@ -9,11 +10,22 @@ moduleForComponent(
   }
 );
 
-test('It throws an error when a data-test-attribute is added on a tagless component', function(assert) {
-  assert.expect(1);
-  assert.throws(function() {
+if (!config.stripTestSelectors) {
+  test('It throws an error when a data-test-attribute is added on a tagless component', function(assert) {
+    assert.expect(1);
+    assert.throws(function() {
+      this.render(
+        hbs`{{data-test-component tagName='' data-test-attribute=true}}`
+      );
+    }, 'Error is thrown properly on tagless component with data-attribute');
+  });
+} else {
+  test('it doest not throw an error when stripTestSelectors is set to true in the config', function(assert) {
     this.render(
-      hbs`{{data-test-component tagName='' data-test-attribute=true}}`
+      hbs`{{data-test-component tagName=''  data-test-attribute=true}}`
     );
-  }, 'Error is thrown properly on tagless component with data-attribute');
-});
+    assert
+      .dom('[data-test-attribute]')
+      .doesNotExist('data-test-attribute was stripped, so no error is thrown');
+  });
+}

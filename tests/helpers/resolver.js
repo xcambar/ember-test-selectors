cbc2887e3d2c6a3f5382398c1dc44615281a7ede
Xavier Cambar <xcambar@gmail.com>
2018-08-30 18:12:54 +0200

    Fixes CI

--- tests/helpers/resolver.js
+++ tests/helpers/resolver.js
@@ -1,7 +1,7 @@
 import Resolver from '../../resolver';
 import config from '../../config/environment';
 
-const resolver = Resolver.create();
+const resolver = Resolver.create({ namespace: 'whatevs' });
 
 resolver.namespace = {
   modulePrefix: config.modulePrefix,

5d3177b31f2ccde1189be7a248a8785a042260d1
mathieupoteriepeopledoc <mathieu.poterie@people-doc.com>
2018-08-30 12:14:47 +0200

    test: edited the bind-data-test-attributes test in order to only check that an error is thrown

--- tests/integration/bind-data-test-attributes-test.js
+++ tests/integration/bind-data-test-attributes-test.js
@@ -13,15 +13,11 @@ moduleForComponent(
 if (!config.stripTestSelectors) {
   test('It throws an error when a data-test-attribute is added on a tagless component', function(assert) {
     assert.expect(1);
-    assert.expectAssertion(
-      () => {
-        this.render(
-          hbs`{{data-test-component tagName='' data-test-attribute=true}}`
-        );
-      },
-      'Assertion Failed: ember-test-selectors could not bind data-test-* properties on <dummy@component:data-test-component::ember495> automatically because tagName is empty.',
-      'Error is thrown properly on tagless component with data-attribute'
-    );
+    assert.expectAssertion(() => {
+      this.render(
+        hbs`{{data-test-component tagName='' data-test-attribute=true}}`
+      );
+    });
   });
 } else {
   test('it doest not throw an error when stripTestSelectors is set to true in the config', function(assert) {
